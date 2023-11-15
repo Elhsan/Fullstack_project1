021de53a596a4b5ca13e71fe328a6fa479cdafad
Elhsan <elshanaliev2009@gmail.com>
2023-11-15 19:26:00 +0500

    Token validation

--- frontend/src/components/NoPage.jsx
+++ frontend/src/components/NoPage.jsx
@@ -1,11 +1,11 @@
 const NoPage = () => {
     return (
         <div>
-            <div className="no-page">
+            <div className='no-page'>
                 <h1>404</h1>
             </div>
         </div>
     )
-}
+};
 
 export default NoPage;
