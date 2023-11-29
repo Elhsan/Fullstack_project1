cb074b4b580ab9c4a94b4002f5e1a12b2e94cfbd
Elhsan <elshanaliev2009@gmail.com>
2023-11-29 18:56:51 +0500

    add Toaster

--- frontend/src/components/AuthControl.jsx
+++ frontend/src/components/AuthControl.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { isLoggedIn } from '../conf/common';
+import { toast } from 'react-toastify';
 
 export default function AuthControl(Component) {
     return function AuthenticatedComponent(props) {
@@ -8,6 +9,7 @@ export default function AuthControl(Component) {
 
         useEffect(() => {
             if (!isLoggedIn()) {
+                toast.error('You are not logged in!', { toastId: 'success1',})
                 navigate('/auth');
             }
         }, [navigate]);
