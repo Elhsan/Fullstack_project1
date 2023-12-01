4ce1ea52cc671f5502ce042d8fb98d3508e03101
Elhsan <elshanaliev2009@gmail.com>
2023-12-01 18:53:36 +0500

    Profile page

--- frontend/src/components/AllComponents.jsx
+++ frontend/src/components/AllComponents.jsx
@@ -4,8 +4,10 @@ import Auth from '../components/Authentication'
 import Navigation from '../components/Navigation'
 import Home from '../components/Home'
 import AuthControl from './AuthControl'; 
+import Profile from './Profile'
 
 const AuthHome = AuthControl(Home)
+const AuthProfile = AuthControl(Profile)
 
 export default function AllComponents() {
   const location = useLocation()
@@ -13,8 +15,9 @@ export default function AllComponents() {
     <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Navigation />} >
         <Route index element={<AuthHome />} />
-        <Route path="*" element={<NoPage />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/profile" element={<AuthProfile />} />
+        <Route path="*" element={<NoPage />} />
       </Route>
     </Routes>
   )
